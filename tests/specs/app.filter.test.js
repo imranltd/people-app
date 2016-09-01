b935dd453bae7fbb47eef4ebc0d93b19774ab846
Imran Sarwar <bitbucket@imransarwar.co.uk>
2016-09-01 14:27:50 +0100

    - added unit test for formatDateFilter - added angular/angular-mocks files to karma.config.js

---
+++ tests/specs/app.filter.test.js
@@ -0,0 +1,18 @@
+describe('Filters', function(){ //describe your object type
+    beforeEach(module('peopleAppFilter')); //load module
+    describe('formatDate',function(){ //describe your app name
+        var formatDate;
+        beforeEach(inject(function($filter){ //initialize your filter
+            formatDate = $filter('formatDate',{});
+        }));
+        it('Should formatDate a string', function(){  //write tests
+            expect(formatDate('2014-08-15T11:34:51 -01:00')).toBe('15 Aug 2014'); //pass
+            expect(formatDate('2015-04-19T05:14:37 -01:00')).toBe('19 Apr 2015'); //pass
+            expect(formatDate('2014-01-09T06:44:29 -00:00')).toBe('09 Jan 2014'); //pass
+            expect(formatDate('2014-04-16T08:49:21 -01:00')).toBe('16 Apr 2014'); //pass
+
+            //expect(formatDate('2014-04-16T08:49:21 -01:00')).toBe('06 Apr 2014'); //fail
+
+        });
+    });
+});
